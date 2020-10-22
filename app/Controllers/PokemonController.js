3cb10a9ce81a91f72cc892c486ad4ea63fb94728
MickShannahan <beasticorn@gmail.com>
2020-10-22 16:04:24 -0600

    set up for fireside

--- app/Controllers/PokemonController.js
+++ app/Controllers/PokemonController.js
@@ -1,6 +1,4 @@
 import PokemonService from "../Services/PokemonService.js";
-import Pokemon from "../Models/Pokemon.js"
-import _store from "../store.js";
 
 //Private
 let _pagenumber = 1;
@@ -8,33 +6,26 @@ let _pagenumber = 1;
 
 function _drawWildPokemon() {
   let template = ''
-  _store.State.wildPokemon.forEach(pokemonName => template += Pokemon.WildPokemonTemplate(pokemonName))
-  document.getElementById('wild-pokemon').innerHTML = template
+  let wildPokemon = document.getElementById('wild-pokemon')
 }
 
 function _drawActivePokemon() {
   // @ts-ignore
-  document.getElementById("active-pokemon").innerHTML = _store.State.activePokemon.ActivePokemonTemplate
+  let activePokemon = document.getElementById("active-pokemon")
 }
 
 function _drawMyPokemon() {
   let template = ''
-  _store.State.myPokemon.forEach(pokemonName => template += pokemonName.MyPokemonTemplate)
-  document.getElementById('my-pokedex').innerHTML = template
+  let myPokeDex = document.getElementById('my-pokedex')
 }
 
-function _drawPageNumber(direction) {
-  direction == "forward" ? _pagenumber++ : direction == "back" ? _pagenumber-- : console.log("out of pages");
-  document.getElementById('page-counter').innerText = `${_pagenumber}/10`
-}
+
 
 
 //Public
 export default class PokemonController {
   constructor() {
-    _store.subscribe("wildPokemon", _drawWildPokemon);
-    _store.subscribe("activePokemon", _drawActivePokemon);
-    _store.subscribe("myPokemon", _drawMyPokemon);
+
   }
 
   getActivePokemon(pokeName) {
@@ -48,9 +39,6 @@ export default class PokemonController {
     PokemonService.releasePokemon(pokemonId)
   }
 
-  changePage(direction) {
-    PokemonService.changePage(direction)
-    _drawPageNumber(direction)
-  }
+
 
 }
--- app/Services/PokemonService.js
+++ app/Services/PokemonService.js
@@ -1,7 +1,4 @@
-import _store from "../store.js";
-import Pokemon from "../Models/Pokemon.js"
 
-let _pageOffset = 0;
 
 // @ts-ignore
 const _pokeApi = axios.create({
@@ -17,49 +14,9 @@ const _bcwApi = axios.create({
 
 class PokemonService {
   constructor() {
-    this.getWildPokemon()
-    this.getMyPokemon()
-  }
-  getWildPokemon() {
-    _pokeApi.get(`pokemon?limit=151&offset=${_pageOffset}`).then(res => {
-      _store.commit("wildPokemon", res.data.results.map(pokemon => pokemon.name))
-    }).catch(err => console.error(err))
+
   }
 
-  changePage(direction) {
-    if (direction == 'back' && _pageOffset != 0) {
-      _pageOffset -= 100
-    } else if (direction == 'forward' && _pageOffset < 864) {
-      _pageOffset += 100
-    } else (console.log("end of pages"))
-    this.getWildPokemon()
-    console.log(_pageOffset);
-  }
-  getActivePokemon(pokeName) {
-    _pokeApi.get("pokemon/" + pokeName).then(res => {
-      console.log(res.data);
-      _store.commit('activePokemon', new Pokemon(res.data))
-    }).catch(err => console.error(err))
-  }
-
-  catchPokemon() {
-    _bcwApi.post("pokemon", _store.State.activePokemon).then(res => {
-      console.log(res);
-      this.getMyPokemon()
-    }).catch(err => console.error(err))
-  }
-
-  getMyPokemon() {
-    _bcwApi.get("pokemon").then(res => {
-      _store.commit("myPokemon", res.data.data.map(pokemon => new Pokemon(pokemon)))
-      console.log(_store.State.myPokemon)
-    }).catch(err => console.error(err))
-  }
-  releasePokemon(pokemonId) {
-    _bcwApi.delete("pokemon/" + pokemonId).then(res => {
-      _store.commit("myPokemon", _store.State.myPokemon.filter(pokemon => pokemon._id !== pokemonId))
-    }).catch(err => console.error(err))
-  }
 }
 
 
